12ea8dcadb81ccea43ee03ebcd4b212b21e3f172
douglaskchen <149521165+douglaskchen@users.noreply.github.com>
2025-01-28 02:12:33 -0500

    Add core files

---
+++ script.js
@@ -0,0 +1,5 @@
+import data from "sample.json" assert { type: 'json' };
+//data = require("sample.json");
+console.log(data);
+
+//let Arr = data;
---
+++ test.py
@@ -0,0 +1,22 @@
+import json
+from opensky_api import OpenSkyApi
+# api = OpenSkyApi()
+# s = api.get_states()
+# print(s)
+
+api = OpenSkyApi()
+# bbox = (min latitude, max latitude, min longitude, max longitude)
+states = api.get_states(bbox=(43.516689, 43.790924, -79.856186, -79.372101))
+# for s in states.states:
+#     print(s)
+for s in states.states:
+    print(s)
+
+# Convert StateVector objects to dictionaries
+state_dicts = [s.__dict__ for s in states.states]
+
+json_object = json.dumps(state_dicts, indent=4)
+ 
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
