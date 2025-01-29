64de9ef3506b2e5bb09a1e3baebe9214733eb339
Douglas <douglaskchen@gmail.com>
2025-01-29 03:36:11 -0500

    1. created leaflet map 2. scraped current aircraft data within a box using opensky api 3. placed aircraft data into json file and read json file contents and objects with javascript 4. worked out local server hosting with python3 -m http.server or inline python code 5. added overlay for wind NEXT STEPS: get live wind data, add aircraft markers with appropriate orientations, run script live with refresh every 10 seconds for wind/aircraft data

--- script.js
+++ script.js
@@ -1,5 +1,63 @@
-import data from "sample.json" assert { type: 'json' };
-//data = require("sample.json");
+import data from "./sample.json" with { type: 'json' };
 console.log(data);
 
-//let Arr = data;
+
+let onething = data[0];
+console.log(onething);
+
+console.log(onething.callsign);
+
+var loc = window.location.pathname;
+console.log(loc);
+
+
+
+
+
+
+function initMap() {
+    var World = L.tileLayer(
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }
+    );
+  
+    var baseLayers = {
+      Default: World,
+    };
+  
+    var map = L.map("map", {
+      layers: [World]
+    });
+  
+    var layerControl = L.control.layers(baseLayers);
+    layerControl.addTo(map);
+    map.setView([43.68, -79.64], 13);
+  
+    return {
+      map: map,
+      layerControl: layerControl
+    };
+  }
+  
+
+  
+  var mapStuff = initMap();
+  var layerControl = mapStuff.layerControl;
+
+  $.getJSON("./leaflet-velocity/demo/wind-global.json",function(data) {
+    var velocityLayer = L.velocityLayer({
+      displayValues: true,
+      displayOptions: {
+        velocityType: "Global Wind",
+        position: "bottomleft",
+        emptyString: "No wind data"
+      },
+      data: data,
+      maxVelocity: 15
+    });
+  
+    layerControl.addOverlay(velocityLayer, "Wind - Global");
+  });
--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 import json
+
 from opensky_api import OpenSkyApi
-# api = OpenSkyApi()
-# s = api.get_states()
-# print(s)
 
 api = OpenSkyApi()
 # bbox = (min latitude, max latitude, min longitude, max longitude)
 states = api.get_states(bbox=(43.516689, 43.790924, -79.856186, -79.372101))
+
 # for s in states.states:
 #     print(s)
-for s in states.states:
-    print(s)
 
 # Convert StateVector objects to dictionaries
 state_dicts = [s.__dict__ for s in states.states]
@@ -18,5 +15,31 @@ state_dicts = [s.__dict__ for s in states.states]
 json_object = json.dumps(state_dicts, indent=4)
  
 # Writing to sample.json
-with open("sample.json", "w") as outfile:
+with open("/home/douglas/repos/FlyWind/sample.json", "w") as outfile:
     outfile.write(json_object)
+
+
+
+
+# import os
+# print("Current Working Directory:", os.getcwd())
+
+# import http.server
+# import socketserver
+
+# class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+#     def end_headers(self):
+#         self.send_cache_headers()
+#         super().end_headers()
+
+#     def send_cache_headers(self):
+#         self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+#         self.send_header("Pragma", "no-cache")
+
+# PORT = 3000
+
+# Handler = CustomHTTPRequestHandler
+
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print(f"Serving at port {PORT}")
+#     httpd.serve_forever()
